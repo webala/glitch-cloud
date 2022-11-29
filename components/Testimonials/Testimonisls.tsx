b46d0b44f1354777a0e50ad5822ccf95a8bab6c5
webala <webd01@protonmail.com>
2022-11-29 09:15:14 +0300

    feat: Testimonials

---
+++ components/Testimonials/Testimonisls.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import style from './Testimonials.module.scss'
+
+function Testimonisls() {
+  return (
+    <div className={style.testimonials}>
+        <h1 className={style.heading}>Our clients speak</h1>
+        <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <div className={style.testimony}>
+                        <div className={style.header}>
+                            <p className={style.name}>@webala</p>
+                            <p>11th June</p>
+                        </div>
+                        <p>He has heart, he has passion</p>
+                    </div>
+                </div>
+                <div className="carousel-item">
+                    <div className={style.testimony}>
+                        <div className={style.header}>
+                            <p className={style.name}>@webala</p>
+                            <p>11th June</p>
+                        </div>
+                        <p>He has heart, he has passion</p>
+                    </div>
+                </div>
+                <div className="carousel-item">
+                    <div className={style.testimony}>
+                        <div className={style.header}>
+                            <p className={style.name}>@webala</p>
+                            <p>11th June</p>
+                        </div>
+                        <p>He has heart, he has passion</p>
+                    </div>
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Testimonisls

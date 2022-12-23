7e173946f0b6c2ad2541dd2ba2b702c014466984
webala <webd01@protonmail.com>
2022-12-23 07:46:54 +0300

    style: home page

--- components/Services/components/Ruracio.tsx
+++ components/Services/components/Ruracio.tsx
@@ -3,66 +3,68 @@ import style from "../Services.module.scss";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 
 function Ruracio() {
-  return (
-    <div className={style.ruracio}>
-      <div className={style.heading}>
-        <h1>Ruracio</h1>
-      </div>
+	return (
+		<div className={style.ruracio}>
+			<div className={style.heading}>
+				<h1>Ruracio</h1>
+			</div>
 
-      <Tabs>
-        <div className={style.tab_list}>
-          <TabList>
-            <Tab>Photography</Tab>
-          </TabList>
-          <TabList>
-            <Tab>Videography</Tab>
-          </TabList>
-        </div>
+			<div className={style.item}>
+				<Tabs>
+					<div className={style.tab_list}>
+						<TabList>
+							<Tab>Photography</Tab>
+						</TabList>
+						<TabList>
+							<Tab>Videography</Tab>
+						</TabList>
+					</div>
 
-        <TabPanels>
-          <TabPanel>
-            <div className={style.package}>
-              <ul className="">
-                <li>1 Photographer</li>
-                <li>Full event photography</li>
-                <li>A3 Photomount (1 piece)</li>
-                <li>A4 Photobook (20 pages)</li>
-              </ul>
-              <div className={style.price}>
-                <p className="">ksh 11000</p>
-              </div>
-            </div>
-          </TabPanel>
-          <TabPanel>
-            <div className={style.package}>
-              <ul className="">
-                <li>1 videographer</li>
-                <li>Drone pilot</li>
-                <li>Full event video coverage</li>
-                <li>1 YouTube teaser</li>
-                <li>Flash drive with photos and edited video</li>
-                <li>Drone services</li>
-              </ul>
-              <div className={style.price}>
-                <p className="">ksh 22000</p>
-              </div>
-            </div>
-          </TabPanel>
-          <TabPanel>
-            <div>
-              <ul className="list-disc">
-                <li>10 retoruched images</li>
-                <li>Make Up</li>
-              </ul>
-              <div className={style.price}>
-                <p className="text-textSecondary">ksh 2500</p>
-              </div>
-            </div>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </div>
-  );
+					<TabPanels>
+						<TabPanel>
+							<div className={style.package}>
+								<ul className="">
+									<li>1 Photographer</li>
+									<li>Full event photography</li>
+									<li>A3 Photomount (1 piece)</li>
+									<li>A4 Photobook (20 pages)</li>
+								</ul>
+								<div className={style.price}>
+									<p className="">ksh 11000</p>
+								</div>
+							</div>
+						</TabPanel>
+						<TabPanel>
+							<div className={style.package}>
+								<ul className="">
+									<li>1 videographer</li>
+									<li>Drone pilot</li>
+									<li>Full event video coverage</li>
+									<li>1 YouTube teaser</li>
+									<li>Flash drive with photos and edited video</li>
+									<li>Drone services</li>
+								</ul>
+								<div className={style.price}>
+									<p className="">ksh 22000</p>
+								</div>
+							</div>
+						</TabPanel>
+						<TabPanel>
+							<div>
+								<ul className="list-disc">
+									<li>10 retoruched images</li>
+									<li>Make Up</li>
+								</ul>
+								<div className={style.price}>
+									<p className="text-textSecondary">ksh 2500</p>
+								</div>
+							</div>
+						</TabPanel>
+					</TabPanels>
+				</Tabs>
+			</div>
+		</div>
+	);
 }
 
 export default Ruracio;

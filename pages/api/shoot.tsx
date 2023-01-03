1a9e5659f8793b74f12f5f11d9ce6e6e5dbc5956
webala <webdspam@gmail.com>
2023-01-03 12:24:04 +0300

    fix: api handler types

--- pages/api/shoot.tsx
+++ pages/api/shoot.tsx
@@ -1,6 +1,7 @@
 /** @format */
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req, res) {
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
      if (req.method === "POST") {
           const body = req.body;
 
